fe3a3b603629edea6549def3bef3d35938df9c96
unknown <nnmthu08@gmail.com>
2023-09-07 16:21:36 +0700

    responsive cart

--- controller/main.js
+++ controller/main.js
@@ -1,21 +1,2 @@
-// xử lý modal 
-
-// $("#register").on( "click", function() {
-//     $('#signIn').modal('hide');  
-// });
-// $("#register").on( "click", function() {
-//     $('#signUp').modal('show');  
-// });
 
 
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
